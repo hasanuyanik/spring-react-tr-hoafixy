992d294b33f44ed767c9e9ff69fd10c8bfb49662
Hasan UYANIK <40677383+hasanuyanik@users.noreply.github.com>
2021-07-04 23:43:18 +0300

    Username kontrolü - Spring

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,12 +1,17 @@
 package com.hoaxify.ws.user;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.shared.GenericResponse;
 
 @RestController
@@ -17,8 +22,16 @@ public class UserController {
 	
 	@PostMapping("/api/1.0/users")
 	@ResponseStatus(HttpStatus.CREATED)
-	public GenericResponse createUser(@RequestBody User user) {
+	public ResponseEntity<?> createUser(@RequestBody User user) {
+		String username = user.getUsername();
+		if(username == null || username.isEmpty()) {
+			ApiError error = new ApiError(400, "Validation Error", "/api/1.0/users");
+			Map<String, String> validationErrors = new HashMap<>();
+			validationErrors.put("username", "Username cannot be null");
+			error.setValidationErrors(validationErrors);
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+		}
 		userService.save(user);
-		return new GenericResponse("User Created");
+		return ResponseEntity.ok(new GenericResponse("User Created"));
 	}
 }
