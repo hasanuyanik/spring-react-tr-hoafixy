e433e07fafe8fcffce642e5dd82cd4a625135666
Hasan UYANIK <40677383+hasanuyanik@users.noreply.github.com>
2021-07-15 15:10:17 +0300

    Kullanıcı Güncelleme işlemi için React-Client'te ilk düzenlemenin yapılması

--- frontend/src/components/ProfileCard.js
+++ frontend/src/components/ProfileCard.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams} from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileImageWithDefault from './ProfileImageWithDefault';
+import { useTranslation } from 'react-i18next';
+import Input from './Input';
 
 const ProfileCard = props => {
+    const [inEditMode, setInEditMode] = useState(false);
+    const [updatedDisplayName, setUpdatedDisplayName] = useState();
     const { username: loggedInUsername } = useSelector((store) => ({username: store.username}));
     const routeParams = useParams();
     const { user } = props;
     const { username, displayName, image } = user;
-  
+    const { t } = useTranslation();
+    useEffect(() => {
+        if(!inEditMode){
+            setUpdatedDisplayName(undefined);
+        }else{
+            setUpdatedDisplayName(displayName);
+        }
+    }, [inEditMode, displayName]);
+    const onClickSave = () => {
+        
+    }
     const pathUsername = routeParams.username;
                 let message = "We cannot edit";
                 if(pathUsername == loggedInUsername){
@@ -25,9 +39,30 @@ const ProfileCard = props => {
                             image={image} />
                         </div>
                         <div className="card-body">
+                            {!inEditMode && (
+                                <>
                             <h3>
                                 {displayName}@{username}
                             </h3>
+                            <button className="btn btn-success d-inline-flex" onClick={() => setInEditMode(true) }>
+                                <i className="material-icons">edit</i>
+                                {t('Edit')}
+                            </button>
+                                </>
+                            )}
+                            {inEditMode && (
+                                <div>
+                                    <Input label={t('Change Display Name')} defaultValue={displayName} onChange={(event) => {setUpdatedDisplayName(event.target.value)}}/>
+                                    <div>
+                                        <button className="btn btn-primary d-inline-flex" onClick={onClickSave}>
+                                            <i className="material-icons">save</i>{t('Save')}
+                                        </button>
+                                        <button className="btn btn-light d-inline-flex ml-1" onClick={() => setInEditMode(false)}>
+                                            <i className="material-icons">close</i>{t('Cancel')}
+                                        </button>
+                                    </div>
+                                </div>
+                            )}
                         </div>
                     </div>
                 );
